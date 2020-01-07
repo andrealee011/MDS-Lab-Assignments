5de9b68befc3ee565fec1f114577f14f521532f4
Andrea Lee <45249215+andrealee011@users.noreply.github.com>
2020-01-07 18:46:24 -0500

    Update naive_bayes_sentiment_analysis.py

--- naive_bayes_sentiment_analysis.py
+++ naive_bayes_sentiment_analysis.py
@@ -1,7 +1,7 @@
 # Assignment from DSCI 571 Lab 2
 # Using naive bayes to perform sentiment analysis on IMDB movie reviews
 
-# Import libaries
+# Import libraries
 import pandas as pd
 import numpy as np
 import altair as alt
@@ -10,37 +10,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 # 1. Load data (dataset from https://www.kaggle.com/utathya/imdb-review-dataset)
-
 imdb = pd.read_csv("data/imdb_master.csv", encoding="latin_1", index_col = 0)
 imdb_df = imdb.query("label != 'unsup'")
 
 # 2. Split data
-
 X = imdb_df['review']
 y = imdb_df['label']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y.to_numpy().ravel(), test_size = 0.2)
 
 # 3. Preprocess data
-
 tv = TfidfVectorizer()
 X_train_tfidf = tv.fit_transform(X_train)
 X_test_tfidf = tv.transform(X_test)
 
 # 4. Fit model
-
 nb_tfidf = MultinomialNB(alpha=1)
 nb_tfidf.fit(X_train_tfidf, y_train)
 nb_tfidf_train_error = 1- nb_tfidf.score(X_train_tfidf, y_train)
 nb_tfidf_test_error = 1- nb_tfidf.score(X_test_tfidf, y_test)
 
 # 5. Calculate error
-
 print('The training error for the tf-idf representation is %.2f.' % nb_tfidf_train_error)
 print('The testing error for the tf-idf representation is %.2f.' % nb_tfidf_test_error)
 
 # 6. Test model on fake reviews
-
 fake_reviews = ['This movie was excellent! The performances were oscar-worthy!',
                'Unbelievably disappointing.', 
                'Full of zany characters and richly applied satire, and some great plot twists',
